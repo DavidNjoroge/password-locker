0411c62ce58e309ddcfeae32620e2e9c4f61914f
David Mwangi <njorogedavid13@gmail.com>
2017-10-13 12:14:43 +0300

    add test to check if a user can login

--- user.py
+++ user.py
@@ -17,3 +17,12 @@ class User:
         method that saves a new instance ie to register
         '''
         User.users_list.append(self)
+    @classmethod
+    def login( cls,name,password):
+        '''
+        method that checks if the user is already register and then login
+        '''
+        for user in User.users_list:
+            if user.name==name and user.password==password:
+                return True
+            return False
--- user_test.py
+++ user_test.py
@@ -9,13 +9,18 @@ class TestUser(unittest.TestCase):
         '''
         setup method to run before each test case
         '''
-        self.the_user=User('David','password')
+        self.the_user=User('david','password')
+        self.the_user1=User('njoroge','drowssap')
+
+
+    def tearDown(self):
+        User.users_list=[]
 
     def test_init(self):
         '''
         test case to test if the object is initialized
         '''
-        self.assertEqual(self.the_user.name,'David')
+        self.assertEqual(self.the_user.name,'david')
         self.assertEqual(self.the_user.password,'password')
     def test_register(self):
         '''
@@ -24,6 +29,17 @@ class TestUser(unittest.TestCase):
         self.the_user.register()
         self.assertEqual(len(User.users_list),1)
 
+    def test_login(self):
+        '''
+        test if you can login you input the username and passord and returns true
+        args:
+            name: the username
+        '''
+        self.the_user.register()
+        self.the_user1.register()
+
+        to_login=User.login('david','password')
+        self.assertTrue(to_login)
 
 
 
