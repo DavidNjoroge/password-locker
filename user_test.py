0850032ca8d71e7a0a865048011ae800b9a598b7
David Mwangi <njorogedavid13@gmail.com>
2017-10-13 11:42:35 +0300

    add test to check if user is initialized properly

--- user_test.py
+++ user_test.py
@@ -5,6 +5,22 @@ class TestUser(unittest.TestCase):
     '''
     class to test the functionality of user class
     '''
+    def setUp(self):
+        '''
+        setup method to run before each test case
+        '''
+        self.the_user=User('David','password')
+
+    def test_init(self):
+        '''
+        test case to test if the object is initialized
+        '''
+        self.assertEqual(self.the_user.name,'David')
+        self.assertEqual(self.the_user.password,'password')
+
+
+
+
 
 if __name__=='__main__':
     unittest.main()
